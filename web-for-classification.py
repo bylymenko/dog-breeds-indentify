2e85e35aa3f78382ef87277fcec2fb67be80de81
bylymenko <bylymenko.anna1619@vu.cdu.edu.ua>
2023-06-05 22:49:41 +0300

    initial commit

---
+++ web-for-classification.py
@@ -0,0 +1,41 @@
+import io
+import streamlit as st
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+import numpy as np
+
+def preprocess_image(img):
+    img = img.resize((100, 100))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Оберіть зображення')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+model = load_model("dog_breeds.h5")
+
+st.title('Класифікація зображень')
+img = load_image()
+result = st.button('Розпізнати зображення')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результати розпізнавання:**')
+    # print_predictions(preds)
+    st.write(type(preds))
+    st.write(preds)

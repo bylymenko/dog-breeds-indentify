e688a63d8276724e528d2ddf2586e4027829a91e
bylymenko <bylymenko.anna1619@vu.cdu.edu.ua>
2023-06-22 14:23:27 +0300

    Changed main file

--- web-for-classification.py
+++ web-for-classification.py
@@ -58,25 +58,6 @@ def load_image():
     #st.write('Належить до породи: ' + dog_breeds[res])    
     #return res
 
-def get_image_url_by_breed(breed):
-    breed = breed.lower()
-    # Словник з URL зображень для кожної породи
-    image_urls = {
-        'Бігль': 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Shemsu_Sotis_Perun.jpg',
-        'Бульдог': 'https://petsi.net/images/dogbreed/french-bulldog.jpg',
-        'Чихуахуа': 'https://sobakovod.club/uploads/posts/2021-12/1640347969_1-sobakovod-club-p-sobaki-chistokrovnoi-chikhuakhua-1.jpg',
-        'Чау-чау': 'https://images.prom.ua/1029598115_w$%7Bwidth%7D_h$%7Bheight%7D_chau-chau.jpg',
-        'Коргі': 'https://sobaky.info/wp-content/uploads/2018/02/1-4.jpg',
-        'Такса': 'https://malinois.com.ua/wp-content/uploads/2022/10/295249245_8470984569594272_6495582147680100542_n.jpg',
-        'Далматин': 'https://petsi.net/images/dogbreed/13.jpg',
-        'Доберман': 'https://static.tildacdn.com/tild3537-3736-4035-b233-323161306165/reddit__the_front_pa.jpg',
-        'Англійський сетер': 'https://sobakovod.club/uploads/posts/2021-12/1639910757_1-sobakovod-club-p-sobaki-angliiskii-setter-sobaka-1.jpg',
-        'Німецька вівчарка': 'https://petadvice.co.ua/wp-content/uploads/2020/02/german-sherpherd2-813x1024.jpg',
-        'Хаскі': 'https://petdiets.ru/image/data/haski-statya.jpeg',
-        'Джек Расел': 'https://zooposhuk.com.ua/wp-content/uploads/2021/11/jack_russel.jpg'
-    }
-
-
 def print_predictions(preds, top_k=3):
     preds2 = preds.copy()
     preds2 = np.squeeze(preds2)
@@ -85,10 +66,9 @@ def print_predictions(preds, top_k=3):
     for i, index in enumerate(top_indexes):
         breed = dog_breeds[index]
         probability = preds2[index]
-        image_urls = get_image_url_by_breed(breed)
         st.write('{}. {} (Ймовірність: {:.2%})'.format(i, breed, probability))
-        st.image(image_urls)
-    #return top_indexes
+        
+    return top_indexes
     
 
 model = load_model("dog_breeds.h5")

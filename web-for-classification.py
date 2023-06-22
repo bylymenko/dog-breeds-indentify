c05b3d2588f3fe7d2a6560188ee59a5460e2ff02
bylymenko <bylymenko.anna1619@vu.cdu.edu.ua>
2023-06-22 20:01:56 +0300

    Changed main file

--- web-for-classification.py
+++ web-for-classification.py
@@ -58,7 +58,14 @@ def load_image():
 #    #st.write('Належить до породи: ' + dog_breeds[res])    
 #    return res
 
-def print_predictions(preds, top_k=3):
+def get_image_url_by_breed(breed):
+    # Замініть цей рядок на шлях до вашої локальної бази даних зображень
+    images_folder = 'C:/Users/user/Desktop/breeds'
+
+    breed_image_path = f'{images_folder}/{breed}.jpg'
+    return breed_image_path
+
+def print_predictions(preds, top_k=5):
     preds2 = preds.copy()
     preds2 = np.squeeze(preds2)
     top_indexes = np.argsort(preds2)[::-1][:top_k]
@@ -69,44 +76,31 @@ def print_predictions(preds, top_k=3):
         st.write('{}. {} (Ймовірність: {:.2%})'.format(i, breed, probability))
     return top_indexes
 
-#def display_predicted_breed(image, breed):
-#    st.write('**Порода собаки з найбільшою ймовірністю:**')
-#    st.image(image, caption=breed, use_column_width=True)
-import sqlite3
-
-def display_predicted_breed_image(breed):
-    # Підключення до бази даних
-    conn = sqlite3.connect('your_database.db')
-    cursor = conn.cursor()
-
-    # Виконання SQL запиту для отримання зображення за породою собаки
-    cursor.execute("SELECT image_path FROM dog_images WHERE breed = ?", (breed,))
-    result = cursor.fetchone()
-
-    # Закриття підключення до бази даних
-    cursor.close()
-    conn.close()
-
-    if result:
-        image_path = result[0]
-        image = Image.open(image_path)
-        st.image(image, caption=f"Зображення породи {breed}")
-    else:
-        st.write("Зображення для цієї породи собаки не знайдено.")
     
 model = load_model("dog_breeds.h5")
 
+#st.title('Класифікація зображень')
+#img = load_image()
+#result = st.button('Розпізнати зображення')
+#if result:
+#    x = preprocess_image(img)
+#    preds = model.predict(x)
+#    st.write('**Результати розпізнавання:**')
+    #st.write(preds)
+    #st.write(type(preds))
+#    print_predictions(preds)
 st.title('Класифікація зображень')
 img = load_image()
 result = st.button('Розпізнати зображення')
+
 if result:
     x = preprocess_image(img)
     preds = model.predict(x)
     st.write('**Результати розпізнавання:**')
-    #top_indexes = print_predictions(preds)
-    #top_breed_index = top_indexes[0]
-    #top_breed = dog_breeds[top_breed_index]
-    #display_predicted_breed(img, top_breed)
-    #st.write(preds)
-    #st.write(type(preds))
+    top_indexes = np.argsort(preds[0])[::-1][:1]
+    top_breed_index = top_indexes[0]
+    top_breed = dog_breeds[top_breed_index]
+    breed_image_path = get_image_url_by_breed(top_breed)
+    breed_image = Image.open(breed_image_path).convert('RGB')
+    st.image(breed_image, caption=top_breed, width=200)
     print_predictions(preds)
